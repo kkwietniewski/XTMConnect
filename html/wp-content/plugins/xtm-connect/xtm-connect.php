72a5859367d84df36c6ae9ed04f11ff45d3fd595
kkwietniewski <kwietniewski.kacperr@gmail.com>
2021-04-03 15:10:07 +0200

    Initial commit

---
+++ html/wp-content/plugins/xtm-connect/xtm-connect.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package XTMConnect
+ */
+/*
+Plugin Name: XTM Connect
+Plugin URI: https://github.com/kkwietniewski/XTMConnect/tree/main/html/wp-content/plugins/xtm-connect
+Description: This is the recruitment task plugin.
+Varsion: 1.0.0
+Author: Kacper Kwietniewski
+Author URI: https://github.com/kkwietniewski/
+License: GPLv3 or later
+Text-Domain: xtm-connect
+*/
+
+/*
+    XTMConnect the recruitment task plugin.
+    Copyright (C) 2021 Kacper Kwietniewski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
