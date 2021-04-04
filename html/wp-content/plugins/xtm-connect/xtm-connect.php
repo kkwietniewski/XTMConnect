b69323ac4aee03aa58d3bf415a464186a6606f1f
kkwietniewski <kwietniewski.kacperr@gmail.com>
2021-04-04 10:34:36 +0200

    vendor path fix, composer added

--- html/wp-content/plugins/xtm-connect/xtm-connect.php
+++ html/wp-content/plugins/xtm-connect/xtm-connect.php
@@ -34,7 +34,7 @@ Text-Domain: xtm-connect
 defined('ABSPATH') or die('Hey, you can\'t access this page!');
 
 if ( file_exists(dirname( __FILE__ ) .'/vendor/autoload.php')){
-	require_once './vendor/autoload.php';
+	require_once dirname( __FILE__ ) .'/vendor/autoload.php';
 }
 
 use Inc\Activate;
